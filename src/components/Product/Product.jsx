b591c92f668b87ba7b68935f86deaf9837443297
unknown <humairakhatun958@gmail.com>
2023-03-29 13:05:51 +0600

    shopping card added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
+
     const {img, name, seller, price, ratings} = props.product;
+    const handleAddToCard = props.handleAddToCard;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +16,10 @@ const Product = (props) => {
                <p>Manufacturer: {seller}</p> 
                <p>Rating: {ratings}star</p>   
             </div> 
-            <button className='btn-card'>Add To Card</button>              
+            <button onClick={() => handleAddToCard(props.product)} className='btn-card'>
+                Add To Card
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>              
 
         </div>
     );
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,12 +4,18 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [card, setCard] = useState([]);
 
     useEffect( () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+
+    const handleAddToCard = (product) => {
+       const newCard = [...card, product];
+       setCard(newCard);
+    }
     return (
         <div className='shop-container'>
            <div className='products-container'>
@@ -17,11 +23,13 @@ const Shop = () => {
                 products.map (product => <Product
                   key={product.id}
                   product={product}
+                  handleAddToCard={handleAddToCard}
                 ></Product>)
             }
            </div>
            <div className="card-container">
              <h4>Order Summary</h4>
+             <p>Selected Items: {card.length}</p>
             </div> 
         </div>
     );
